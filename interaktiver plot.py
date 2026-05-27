58dce89e3fe01d8815b58fe0d5cc5549f2dbf51e
Viktor Pantushev <vik.pa@gmx.com>
2026-05-27 09:56:16 +0200

    fixing the interactve html

--- interaktiver plot.py
+++ interaktiver plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 
 # Charger les données
-df = pd.read_csv("countries_gender_counts.csv")
+df = pd.read_csv("processed_data/countries_gender_counts.csv")
 
 # Nettoyer / préparer
 df["total"] = df["male"] + df["female"] + df["unknown"]
@@ -10,7 +10,7 @@ top10 = df.sort_values("total", ascending=False).head(10)
 
 # Transformer les colonnes male/female/unknown en format long
 df_long = top10.melt(
-    id_vars=["Country/countries", "total"],
+    id_vars=["Country", "total"],
     value_vars=["male", "female", "unknown"],
     var_name="Gender",
     value_name="Count"
@@ -19,7 +19,7 @@ df_long = top10.melt(
 # Graphique interactif
 fig = px.bar(
     df_long,
-    x="Country/countries",
+    x="Country",
     y="Count",
     color="Gender",
     title="Interactive Gender Distribution in Unicorn Startups by Country",
@@ -46,7 +46,7 @@ fig.update_layout(
 fig.update_traces(textposition="inside")
 
 # Sauvegarde en HTML interactif
-fig.write_html("interactive_gender_distribution.html")
+fig.write_html("processed_data/interactive_gender_distribution.html")
 
 # Afficher dans le navigateur
 fig.show()
